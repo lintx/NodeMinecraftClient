7e03b3ebf2ffad85aa44e2680d33d727af059f28
LinTx <lintx@lintx.org>
2018-11-12 21:01:27 +0800

    尝试解决sql连接失败

--- app/server/server/db.js
+++ app/server/server/db.js
@@ -1,3 +1,19 @@
-global.db = require('mysql').createConnection(require('../../config/db'));
-global.db.connect();
+if (!global.db) {
+    let pool = require('mysql').createPool(require('../../config/db'));
+    // global.db.connect();
+    global.db = {
+        query:function (sql, arr, callback) {
+            pool.getConnection(function (err, connection) {
+                if (err) {
+                    console.log(err);
+                    return;
+                }
+                connection.query(sql,arr,function (error, results, fields) {
+                    connection.release();
+                    callback(error,results,fields);
+                });
+            });
+        }
+    };
+}
 
